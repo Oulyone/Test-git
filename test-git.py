1c13f6ce3c1f75ea6f4945485b968063c372fc18
Oulyone <nattapong.ccwr@gmail.com>
2022-10-17 11:26:13 +0700

    Add comment

--- test-git.py
+++ test-git.py
@@ -1,3 +1,9 @@
+from cgi import test
+from importlib.resources import path
+from msilib.schema import Directory
+from telnetlib import STATUS
+
+
 def hello(time):
     for count in range(1,time):
         print('hello')
@@ -7,3 +13,17 @@ except ValueError as err:
     print(err)
 
 hello(time)
+
+
+# git clone https://github.com/Oulyone/Test-git.git
+
+# get in Test-git path
+
+# git status
+
+# git add test-git.py
+
+# git commit -m"message"
+
+# git push
+
